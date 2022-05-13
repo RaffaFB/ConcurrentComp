e104a36ca735518b244d4dff964e3ca9bb321c11
Rafaella Busch <44062212+RaffaFB@users.noreply.github.com>
2022-05-13 19:30:14 -0300

    Update lock.c

--- LAB4/lock.c
+++ LAB4/lock.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <math.h>
-//#include "timer.h"
+#include "timer.h"
 
 int s = 0;
 pthread_mutex_t mutex;
@@ -15,18 +15,28 @@ void *tarefa(void* threadid){
   long int tamBloco = dim/nthreads;
   long int ini = id * tamBloco;
   long int fim;
+  float entrada, saida;
+
   if(id == nthreads-1) fim = dim;
   else fim = ini + tamBloco;
 
+  printf("rodando thread %d\n", id);
   for(i=ini; i<fim; i++){
+
     pthread_mutex_lock(&mutex);
-    if ((ehPrimo(vetEntrada[i])) == 1){
-      vetSaida[i] = sqrt(vetEntrada[i]);
-    }else{
-      vetSaida[i] = vetEntrada[i];
-    }
-    printf("vetor concorrente %lf\n", vetSaida[i]);
+    entrada = vetEntrada[i];
     pthread_mutex_unlock(&mutex);
+
+    if ((ehPrimo(vetEntrada[i])) == 1){
+      saida = sqrt(entrada);
+    }else{
+      saida = entrada;
+    }
+
+    pthread_mutex_lock(&mutex);
+    vetSaida[i] = saida;
+    pthread_mutex_unlock(&mutex);
+    //printf("vetor concorrente %lf\n", vetSaida[i]);
   }
   pthread_exit(NULL);
 }
@@ -41,7 +51,7 @@ int * preencheVetor(int dim){
   }
   for(i=0; i<dim; i++){
      v[i] = rand() % 9;
-     printf("%d\n", v[i]);
+     //printf("%d\n", v[i]);
   }
   return v;
 }
@@ -64,13 +74,14 @@ void processaPrimos(int * vetorEntrada, float * vetorSaida, int dim) {
       }else{
         vetorSaida[i] = vetorEntrada[i];
       }
-      printf("Vetor sequencial: %lf\n", vetSaida[i]);
+      //printf("Vetor sequencial: %lf\n", vetSaida[i]);
   }
 }
 
 int main(int argc, char const *argv[]) {
   pthread_t* tid;
   int *i;
+  float ini, fim, tempoSeq, tempoConc;
   if(argc < 3) {
       fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
       return 1;
@@ -83,14 +94,25 @@ int main(int argc, char const *argv[]) {
      fprintf(stderr, "ERRO--malloc\n");
      return 2;
   }
-  vetEntrada = preencheVetor(dim);
-  processaPrimos(vetEntrada, vetSaida, dim);
 
+  vetEntrada = preencheVetor(dim);
+
+  GET_TIME(ini);
+  processaPrimos(vetEntrada, vetSaida, dim);
+  GET_TIME(fim);
+  tempoSeq = fim - ini;
+  printf("tempo sequencial: %lf\n", tempoSeq);
+
+
+
+  GET_TIME(ini);
   tid = (pthread_t*) malloc(sizeof(int)*nthreads);
   if(tid==NULL) {
      fprintf(stderr, "ERRO--malloc\n");
      return 2;
   }
+
+  pthread_mutex_init(&mutex, NULL);
 
   for(long int i=0; i<nthreads; i++) {
     if(pthread_create(tid+i, NULL, tarefa, (void*) i)){
@@ -105,6 +127,11 @@ int main(int argc, char const *argv[]) {
         return 3;
      }
   }
+  pthread_mutex_destroy(&mutex);
+  
+  GET_TIME(fim);
+  tempoConc = fim-ini;
+  printf("tempo concorrente: %lf\n", tempoConc);
+  printf("ganho de desempenho %lf\n", tempoSeq/tempoConc);
   return 0;
 }
-
